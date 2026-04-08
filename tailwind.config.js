ae912a5c152d3372aa5164dfccebbfca9e3768c8
FebrianPujoL <78261758+FebrianPujoL@users.noreply.github.com>
2026-04-09 02:41:42 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,jsx,ts,tsx,html}', // Make sure all extensions are included
+    ],
+    // ...
+  }
+  
